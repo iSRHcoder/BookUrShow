0560a81be6fa6c440bdd7531fe48519019e8391d
iSRHcoder <s.r.hatkadke@gmail.com>
2024-01-04 15:46:13 +0530

    api is showing 2023 movies

--- src/services/movie.js
+++ src/services/movie.js
@@ -1,11 +1,18 @@
-export const getMovieList = (page = 1) => {
-  return fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=a9b9fc20b24e7aac267be09f72d57e26&page=${page}&region=IN&primary_release_year=2024`
-  ).then((res) => res.json());
-};
+//2024
+// export const getMovieList = (page = 1) => {
+//   return fetch(
+//     `https://api.themoviedb.org/3/discover/movie?api_key=a9b9fc20b24e7aac267be09f72d57e26&page=${page}&region=IN&primary_release_year=2024`
+//   ).then((res) => res.json());
+// };
 
 //2024 Hindi : https://api.themoviedb.org/3/discover/movie?api_key=a9b9fc20b24e7aac267be09f72d57e26&page=1&with_original_language=hi&primary_release_year=2024
-//2023 All : https://api.themoviedb.org/3/discover/movie?api_key=a9b9fc20b24e7aac267be09f72d57e26&page=1&with_original_language=hi&primary_release_year=2023&region=IN
+
+//2023
+export const getMovieList = (page = 1) => {
+  return fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=a9b9fc20b24e7aac267be09f72d57e26&page=${page}&with_original_language=hi&primary_release_year=2023&region=IN`
+  ).then((res) => res.json());
+};
 
 export const getMovieDetails = (mid) => {
   return fetch(
